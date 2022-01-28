ad9343230c2dc7c91f9b3642940aa82464de778a
Blanchard <pub.coneri@gmail.com>
2022-01-28 10:17:57 +0100

    init spring name Filtre

---
+++ Filtre/src/main/java/com/projetFrame/Filtre/FiltrePokeApplication.java
@@ -0,0 +1,13 @@
+package com.projetFrame.Filtre;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class FiltrePokeApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(FiltrePokeApplication.class, args);
+	}
+
+}
---
+++ Filtre/src/test/java/com/projetFrame/Filtre/FiltrePokeApplicationTests.java
@@ -0,0 +1,13 @@
+package com.projetFrame.Filtre;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class FiltrePokeApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
